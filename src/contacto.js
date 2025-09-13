54ad157ef60eee6d5341e6d36959a72bd4053f5a
Serena <rotondaserena3@gmail.com>
2025-09-13 12:22:51 -0300

    aa

--- src/contacto.js
+++ src/contacto.js
@@ -6,3 +6,35 @@
 */
 
 //Contacto.js: clase Contacto con propiedades nombre, telefono, email. */
+const fs = require("fs");
+class contactos {
+    constructor(nombre,apellido,numero,mail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+        this.mail = mail;
+    }
+    mostrarcontactos(){
+        return(`Nombre: ${this.nombre}, Apellido: ${this.apellido}, Numero de telefono: ${this.numero}, Mail: ${this.mail}`);
+    }
+}
+
+const persona1 = new contactos("Flora","Rodriguez","2494222119","floraRodriguez22@gmail.com");
+const arrayDeContactos = [persona1];
+const textoDeConstactos = arrayDeContactos.map(f => `Nombre: ${f.nombre}, Apellido: ${f.apellido}, Numero de telefono: ${f.numero}, Mail: ${f.mail}`).join("\n");
+fs.writeFile("contactos.txt",textoDeConstactos, (error)=>{
+    if(error){
+        console.error("Error al leer el archivo");
+        return;
+    }
+    console.log("Archivo contactos, creado con exito!");
+    fs.readFile("contactos.txt","utf8",(error,data) => {
+        if(error){
+            console.error("error al leer el Archivo", error);
+            return;
+        }
+        console.log("\nContenido del archivo:");
+        console.log(data);
+    });
+
+});

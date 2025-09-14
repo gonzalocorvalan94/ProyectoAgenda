bb8cc71d8e9c691255acd02b4b114633f736b305
Gonzalo Corvalan <gonzalo.corvalan77@gmail.com>
2025-09-14 20:29:19 -0300

    Cambio a agenda.json, cargar y leer la db. Cambios de estructura de carpetas

--- cli/menu.js
+++ cli/menu.js
@@ -1,7 +1,12 @@
-import chalk from "chalk";
-import PromptSync from "prompt-sync";
-import { validarOpcion } from "../validators/contactoValidator.js";
-
+import chalk from 'chalk';
+import PromptSync from 'prompt-sync';
+import { validarOpcion } from '../validators/contactoValidator.js';
+import { Contactos } from '../src/contacto.js';
+import {
+  crearContactos,
+  actualizarContacto,
+  eliminarContacto,
+} from '../service/contactosService.js';
 const prompt = PromptSync();
 
 export function menu() {
@@ -10,7 +15,8 @@ export function menu() {
   while (!salir) {
     console.log(chalk.blue(`=== AGENDA DE CONTACTOS ===`));
 
-    console.log(chalk.blue(`
+    console.log(
+      chalk.blue(`
       Menu principal
 
       1.- Listar contactos
@@ -18,37 +24,35 @@ export function menu() {
       3.- Actualizar contacto
       4.- Eliminar contacto
       5.- Salir
-    `));
+    `)
+    );
 
-    let opcion = Number(prompt(chalk.blue("Elige una opción: ")));
+    let opcion = Number(prompt(chalk.blue('Elige una opción: ')));
 
     if (!validarOpcion(opcion)) {
-      console.log(chalk.red("Opción inválida"));
+      console.log(chalk.red('Opción inválida'));
       continue; // vuelve a mostrar el menú
     }
 
     switch (opcion) {
       case 1:
-        console.log("Listar"); //Aca tendria que ir la funcion correspondiente
+        console.log('Listar'); //Aca tendria que ir la funcion correspondiente
         break;
       case 2:
-        console.log("Agregar"); //Aca tendria que ir la funcion correspondiente
+        crearContactos();
         break;
       case 3:
-        console.log("Actualizar"); //Aca tendria que ir la funcion correspondiente
+        actualizarContacto(); //Aca tendria que ir la funcion correspondiente
         break;
       case 4:
-        console.log("Eliminar"); //Aca tendria que ir la funcion correspondiente
+        eliminarContacto(); //Aca tendria que ir la funcion correspondiente
         break;
       case 5:
-        console.log(chalk.green("Hasta luego")); //Aca termina, no necesita funcion
+        console.log(chalk.green('Hasta luego')); //Aca termina, no necesita funcion
         salir = true;
         break;
     }
   }
 }
-
-
-
 
 menu();

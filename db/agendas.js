1250411a2f276e6dcd7a94ff12840abd9a2e1481
Serena <rotondaserena3@gmail.com>
2025-09-14 13:10:45 -0300

    subiendo 20 contactos a la agenda

---
+++ db/agendas.js
@@ -0,0 +1,24 @@
+//base de datos
+
+export let agenda = [
+  { nombre: "Susana", numero: "2494001234", mail: "susana@gmail.com" },
+  { nombre: "Juan", numero: "2494012345", mail: "juan@gmail.com" },
+  { nombre: "Ana", numero: "2494023456", mail: "ana@gmail.com" },
+  { nombre: "Carlos", numero: "2494034567", mail: "carlos@gmail.com" },
+  { nombre: "María", numero: "2494045678", mail: "maria@gmail.com" },
+  { nombre: "Esteban", numero: "2494056789", mail: "esteban@gmail.com" },
+  { nombre: "Valeria", numero: "2494067890", mail: "valeria@gmail.com" },
+  { nombre: "Federico", numero: "2494078901", mail: "federico@gmail.com" },
+  { nombre: "Julieta", numero: "2494089012", mail: "julieta@gmail.com" },
+  { nombre: "Martín", numero: "2494090123", mail: "martin@gmail.com" },
+  { nombre: "Laura", numero: "2494101234", mail: "laura@gmail.com" },
+  { nombre: "Diego", numero: "2494112345", mail: "diego@gmail.com" },
+  { nombre: "Carolina", numero: "2494123456", mail: "carolina@gmail.com" },
+  { nombre: "Sofía", numero: "2494134567", mail: "sofia@gmail.com" },
+  { nombre: "Nicolás", numero: "2494145678", mail: "nicolas@gmail.com" },
+  { nombre: "Patricia", numero: "2494156789", mail: "patricia@gmail.com" },
+  { nombre: "Sebastián", numero: "2494167890", mail: "sebastian@gmail.com" },
+  { nombre: "Lorena", numero: "2494178901", mail: "lorena@gmail.com" },
+  { nombre: "Matías", numero: "2494189012", mail: "matias@gmail.com" },
+  { nombre: "Gabriela", numero: "2494190123", mail: "gabriela@gmail.com" },
+];
